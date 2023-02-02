25d7b676642b6c57da22251bcdad76011a2ed84f
Armando Jr <elfiservice@hotmail.com>
2023-02-02 08:10:43 -0300

    more stylization

--- app/src/main/java/br/com/ajudanaweb/mygithubprofile/MainActivity.kt
+++ app/src/main/java/br/com/ajudanaweb/mygithubprofile/MainActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.ajudanaweb.mygithubprofile.ui.theme.MyGithubProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,12 +45,59 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyBasicInformations() {
-    Row {
-        Image(painter = painterResource(R.drawable.my_github_profile), contentDescription = "My image profile")
-        Column() {
-            Text(text = "Armando Jr.")
-            Text(text = "elfiservice")
-            Text(text = "Mobile developer at Descartes Inc.")
+    Column() {
+        val boxHeight = remember {
+            150.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Color(0xFF2d333b),
+                    shape = RoundedCornerShape(
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
+                    )
+                )
+                .height(boxHeight)
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.my_github_profile),
+                contentDescription = "My image profile",
+                modifier = Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape),
+            )
+        }
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+        Column(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Armando Jr.",
+                style = MaterialTheme.typography.h4
+            )
+            Text(text = "elfiservice",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold)
+            Text(text = "Mobile developer at Descartes Inc.",
+                Modifier
+                    .padding(
+                        start = 8.dp,
+                        bottom = 8.dp,
+                        end = 8.dp
+                    )
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center)
         }
     }
 }
